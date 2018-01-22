dae649a7a3ec187e1ee1fcab1685d177822ccf87
Ben MacKinnon <bilmackinnon@googlemail.com>
2018-01-22 16:11:24 +0000

    Set up model loading and placing/removing via touch

--- ARSceneKitTutorial/ViewController.swift
+++ ARSceneKitTutorial/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var nodeModel:SCNNode!
+    let nodeName = "cherub"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,12 +25,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.antialiasingMode = .multisampling4X
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        //load custom scene model
+        let modelScene = SCNScene(named: "art.scnassets/cherub.dae")!
+        
+        nodeModel = modelScene.rootNode.childNode(withName: nodeName, recursively: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,6 +62,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //get the location in the sceneView
+        let location = touches.first!.location(in: sceneView)
+        
+        //hit test for objects
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        let hitResults: [SCNHitTestResult] = sceneView.hitTest(location, options: hitTestOptions)
+        
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                node.removeFromParentNode()
+                return
+            }
+        }
+        
+        
+        //hit test for feature points
+        let hitResultsFeaturePoints: [ARHitTestResult] = sceneView.hitTest(location, types: .featurePoint)
+        if let hit = hitResultsFeaturePoints.first {
+            sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+        }
+    }
+    
+    
+    //recursively checks the node and it's parents to see if it's the one we are looking for
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
 
     // MARK: - ARSCNViewDelegate
     
@@ -62,6 +111,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                
+                //Add as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
